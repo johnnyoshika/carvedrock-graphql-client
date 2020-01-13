b759d27632d9a8c46962c685b9c6964c10c78fa5
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-12 21:52:49 -0800

    Add layout in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const App = () => (
-  <div>
-    Hello world!
+  <div className="container">
+      <header className="row">
+          <div className="col-md-5 mb-4">
+              <img height="150" src="/images/carved-rock-logo.png" alt="logo"/>
+          </div>
+          <div className="col-md-7 mt-5">
+              For outdoorsy types
+          </div>
+      </header>
+      <div>Outlet...</div>
   </div>
 );
 
