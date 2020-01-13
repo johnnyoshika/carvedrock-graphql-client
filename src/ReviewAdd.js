2f4dd7b030d8be409a8f9143634038d707f09791
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-13 09:09:52 -0800

    Add mutation in ReviewAdd

--- src/ReviewAdd.js
+++ src/ReviewAdd.js
@@ -1,18 +1,84 @@
 import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 
-const ReviewAdd = () => {
+import Error from './Error';
+
+import { GET_PRODUCT } from './queries';
+import { PRODUCT_REVIEW_TYPE_FRAGMENT } from './fragments';
+
+const ADD_REVIEW = gql`
+  mutation($review: ReviewInput!) {
+    createReview(review: $review) {
+      ...review
+    }
+  }
+  ${PRODUCT_REVIEW_TYPE_FRAGMENT}
+`;
+
+const updateReviews = (
+  client,
+  {
+    data: {
+      createReview: review
+    }
+  },
+  productId
+) => {
+  const data = client.readQuery({
+    query: GET_PRODUCT,
+    variables: {
+      id: productId
+    }
+  });
+
+  client.writeQuery({
+    query: GET_PRODUCT,
+    variables: {
+      id: productId
+    },
+    data: {
+      ...data,
+      product: {
+        ...data.product,
+        reviews: [
+          ...data.product.reviews,
+          review
+        ]
+      }
+    }
+  });
+};
+
+const ReviewAdd = ({ match: { params: { id: productId } } }) => {
   const [title, setTitle] = useState('');
   const [review, setReview] = useState('');
+
+  const [addReview, { error }] = useMutation(ADD_REVIEW, {
+    variables: {
+      review: {
+        title: title,
+        review: review,
+        productId
+      }
+    },
+    update: (client, data) => updateReviews(client, data, productId)
+  });
 
   const onTitleChange = e => setTitle(e.target.value);
   const onReviewChange = e => setReview(e.target.value);
 
   const onSubmit = e => {
     e.preventDefault();
+    addReview().then(() => {
+      setTitle('');
+      setReview('');
+    });
   };
 
   return (
     <div className="mb-5">
+      {error && <Error error={error} />}
       <h4>Add a review</h4>
       <form onSubmit={onSubmit}>
         <div className="row mb-2 form-group">
