d41dcac64ecc5792c91a1ff427a9ff67c37497ab
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-12 23:12:51 -0800

    Implement Product component

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
-const Product = () => (
-  <div>
-    Product
-  </div>
-);
+import Spinner from './Spinner';
+import Error from './Error';
+
+const GET_PRODUCT = gql`
+  query ($id: ID!) {
+    product(id: $id) {
+      id
+      name
+      price
+      rating
+      photoFileName
+      description
+      stock
+      introducedAt
+      reviews {
+        id
+        title
+        review
+      }
+    }
+  }
+`;
+
+const Product = ({ match: { params: { id } } }) => {
+  const { data, loading, error } = useQuery(GET_PRODUCT, {
+    variables: {
+      id
+    }
+  });
+
+  if (error) return <Error error={error} />;
+
+  if (loading && !data) return <Spinner />;
+
+  const { product } = data;
+
+  return (
+    <div className="row">
+      <div className="col-3">
+        <img height="150" src={'/images/' + product.photoFileName} alt={product.name} />
+      </div>
+      <div className="col-9">
+        <div className="row">
+          <div className="col-12">
+            <h3>{product.name}</h3>
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-12">
+            {product.description}}
+          </div>
+        </div>
+        <div className="row mb-4">
+          <div className="col-3">
+            In store since: {product.introducedAt}
+          </div>
+          <div className="col-3">
+            Stock: {product.stock}
+          </div>
+          <div className="col-3">
+            Stars: {product.rating}
+          </div>
+          <div className="col-3">
+            Price: ${product.price}
+          </div>
+        </div>
+        <h6>Reviews:</h6>
+        <ul></ul>
+        {product.reviews.map(review => (
+          <div key={review.id}>
+            <div className="row">
+              <div className="col-12"><h5>{review.title}</h5></div>
+            </div>
+            <div className="row mb-2">
+              <div className="col-12">{review.review}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Product;
