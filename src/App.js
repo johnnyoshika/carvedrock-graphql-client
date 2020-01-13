9930326a243bb0c46e04533ecd7cd0841e04458a
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-12 23:13:04 -0800

    Add link back to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Products from './Products';
 import Product from './Product';
@@ -7,19 +7,23 @@ import ReviewAdd from './ReviewAdd';
 
 const App = () => (
   <div className="container">
+    <Router>
       <header className="row">
           <div className="col-md-5 mb-4">
+            <Link to="/">
               <img height="150" src="/images/carved-rock-logo.png" alt="logo"/>
+            </Link>
           </div>
           <div className="col-md-7 mt-5">
               For outdoorsy types
           </div>
       </header>
-      <Router>
+      <>
         <Route exact path="/" component={Products} />
         <Route path="/products/:id" component={Product} />
         <Route path="/products/:id/review" component={ReviewAdd} />
-      </Router>
+      </>
+    </Router>
   </div>
 );
 
